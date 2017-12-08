45bed90b26dac8d84cc4b6dcf928a13c7f0ccc92
Thapanee T <tewaritruangsri_t@silpakorn.edu>
2017-12-08 20:58:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/locatsu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.locatsu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TO_MANG on 7/12/2560.
+ */
+
+public class MainFirst extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first);
+    }
+}
